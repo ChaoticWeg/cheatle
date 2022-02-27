6a4eda97ad139701384ff94f75ab43eaab8fb493
Shawn <shawn@chaoticweg.cc>
2022-02-26 19:56:25 -0600

    bugfix: don't do anything if letters are full

--- store/BoardState/Letter.tsx
+++ store/BoardState/Letter.tsx
@@ -3,6 +3,11 @@ import { BoardState, BoardStateLetterAction } from "../../typings/RowState";
 export function BoardStateLetterReducer(state: BoardState, action: BoardStateLetterAction) {
     const { letter } = action.payload;
     const { index } = state;
+
+    if (index >= state.tiles.length) {
+        return state;
+    }
+
     const tiles = [
         ...state.tiles.slice(0, index),
         { ...state.tiles[index], letter },
