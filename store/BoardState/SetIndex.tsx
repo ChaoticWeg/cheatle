675f072f72f413eb83a3de5d95e9cbcf1de9ce3a
Shawn <shawn@chaoticweg.cc>
2022-02-27 11:00:59 -0600

    bugfix: set active row's tile index to 0 on blur

--- store/BoardState/SetIndex.tsx
+++ store/BoardState/SetIndex.tsx
@@ -5,14 +5,36 @@ export function BoardStateIndexReducer(
     action: BoardStateIndexAction
 ): BoardState {
     const { index, row: rowIndex } = (action as BoardStateIndexAction).payload;
+    const { activeRow } = state;
+
+    const isSameRow = rowIndex === activeRow;
+    if (isSameRow) {
+        return {
+            ...state,
+            rows: [
+                ...state.rows.slice(0, activeRow),
+                { ...state.rows[activeRow], index },
+                ...state.rows.slice(activeRow + 1)
+            ]
+        };
+    }
+
+    // Set the "from" row index to zero
+    // Need to determine which of the two ["from", "to"] row indices is lesser,
+    // to preserve the order of rows
+    const lowerRowIndex = Math.min(activeRow, rowIndex);
+    const upperRowIndex = Math.max(activeRow, rowIndex);
+    const isFromRowUpper = lowerRowIndex === activeRow;
 
     return {
         ...state,
         activeRow: rowIndex,
         rows: [
-            ...state.rows.slice(0, rowIndex),
-            { ...state.rows[rowIndex], index },
-            ...state.rows.slice(rowIndex + 1)
+            ...state.rows.slice(0, lowerRowIndex),
+            { ...state.rows[lowerRowIndex], index: isFromRowUpper ? 0 : index },
+            ...state.rows.slice(lowerRowIndex + 1, upperRowIndex),
+            { ...state.rows[upperRowIndex], index: isFromRowUpper ? index : 0 },
+            ...state.rows.slice(upperRowIndex + 1)
         ]
     };
 }
