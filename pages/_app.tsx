f9fa97c4be3f77fb8694d2033d7c73770f2e9452
Shawn <shawn@chaoticweg.cc>
2022-02-28 21:32:30 -0600

    add opengraph tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,6 +15,16 @@ function MyApp(props: AppProps) {
                     name="viewport"
                     content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1"
                 />
+
+                {/* OpenGraph tags */}
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://cheatle.chaoticweg.cc" />
+                <meta property="og:title" content="cheatle" />
+                <meta
+                    property="og:description"
+                    content="ruining the competitive spirit of wordle, for fun"
+                />
+
                 <title>cheatle</title>
             </Head>
             <main>
